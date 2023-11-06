699788d9f4087e15e732e016c8a7f8bb6b7b94cc
Vlad Morozov <v@morosoff.ru>
2023-11-06 18:19:46 +0300

    add 100% tests coverage

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -6,6 +6,7 @@ import pytest
 @pytest.mark.parametrize('array, index, default, expected', [
     ([1, 2, 3], 1, None, 2),
     ([], 0, "test", "test"),
+    ([1, 2], 1, None, 2),
 ])
 def test_get(array, index, default, expected):
     assert arrs.get(array, index, default) == expected
@@ -15,6 +16,7 @@ def test_get(array, index, default, expected):
     ([1, 2, 3, 4], 1, 3, [2, 3]),
     ([1, 2, 3], 1, None, [2, 3]),
     ([], None, None, []),
+    ([1, 2, 3, 4], 3, None, [4]),
     ([1, 2, 3], None, None, [1, 2, 3])
 ])
 def test_slice(coll, start, end, expected):
